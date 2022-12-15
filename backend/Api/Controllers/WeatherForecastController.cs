d95118434dc8a02b07e2e20a89e56aecc9ecce60
Vimal Sivasubramanian <vimal.sivasubramanian@maersk.com>
2022-12-15 15:47:47 +0530

    changes

--- backend/Api/Controllers/WeatherForecastController.cs
+++ backend/Api/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching alias Chennai"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
